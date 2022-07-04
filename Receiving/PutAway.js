603f4b91a33c9775db9a2804265a309df378e30b
pollaha <pollapha@gmail.com>
2022-07-04 14:18:28 +0700

    save put away

--- Receiving/PutAway.js
+++ Receiving/PutAway.js
@@ -1,75 +1,208 @@
-var header_PutAway = function()
-{
-	var menuName="PutAway_",fd = "Receiving/"+menuName+"data.php";
+var header_PutAway = function () {
+    var menuName = "PutAway_", fd = "Receiving/" + menuName + "data.php";
 
-    function init()
-    {
-        
+    function init() {
+
     };
 
-    function ele(name)
-    {
+    function ele(name) {
         return $$($n(name));
     };
 
-    function $n(name)
-    {
-        return menuName+name;
+    function $n(name) {
+        return menuName + name;
     };
-    
-    function focus(name)
-    {
-        setTimeout(function(){ele(name).focus();},100);
+
+    function focus(name) {
+        setTimeout(function () { ele(name).focus(); }, 100);
     };
-    
-    function setView(target,obj)
-    {
+
+    function setView(target, obj) {
         var key = Object.keys(obj);
-        for(var i=0,len=key.length;i<len;i++)
-        {
+        for (var i = 0, len = key.length; i < len; i++) {
             target[key[i]] = obj[key[i]];
         }
         return target;
     };
 
-    function vw1(view,name,label,obj)
-    {
-        var v = {view:view,required:true,label:label,id:$n(name),name:name,labelPosition:"top"};
-        return setView(v,obj);
+    function vw1(view, name, label, obj) {
+        var v = { view: view, required: true, label: label, id: $n(name), name: name, labelPosition: "top" };
+        return setView(v, obj);
     };
 
-    function vw2(view,id,name,label,obj)
-    {
-        var v = {view:view,required:true,label:label,id:$n(id),name:name,labelPosition:"top"};
-        return setView(v,obj);
+    function vw2(view, id, name, label, obj) {
+        var v = { view: view, required: true, label: label, id: $n(id), name: name, labelPosition: "top" };
+        return setView(v, obj);
     };
 
-	return {
+    function setTable(tableName, data) {
+        if (!ele(tableName)) return;
+        ele(tableName).clearAll();
+        ele(tableName).parse(data);
+        ele(tableName).filterByAll();
+    };
+
+    function loadData(btn) {
+        var obj = ele('form1').getValues();
+        ajax(fd, obj, 1, function (json) {
+            setTable('dataT1', json.data);
+            ele('GRN_Number').setValue('');
+            ele('Package_Number').setValue('');
+            ele('Location_Code').setValue('');
+        }, btn);
+    };
+
+    return {
         view: "scrollview",
         scroll: "native-y",
-        id:"header_PutAway",
-        body: 
+        id: "header_PutAway",
+        body:
         {
-        	id:"PutAway_id",
-        	type:"clean",
-    		rows:
-    		[
-    		    {
+            id: "PutAway_id",
+            type: "clean",
+            rows:
+                [
+                    {
+                        view: "form",
+                        id: $n("form1"),
+                        on:
 
-                }
-            ],on:
+                        {
+
+                            "onSubmit": function (view, e) {
+
+                                if (view.config.name == 'Package_Number') {
+
+                                    view.blur();
+
+                                    ajax(fd, ele('form1').getValues(), 1, function (json) {
+
+                                        //complete
+
+                                    }, null,
+
+                                        function (json) {
+
+                                            //error
+
+                                        });
+
+                                }
+                                if (view.config.name == 'Location_Code') {
+
+                                    view.blur();
+                                    var obj = ele('form1').getValues();
+                                    ajax(fd, obj, 21, function (json) {
+                                        setTable('dataT1', json.data);
+                                        ele('GRN_Number').setValue('');
+                                        ele('Package_Number').setValue('');
+                                        ele('Location_Code').setValue('');
+                                        webix.UIManager.setFocus(ele('GRN_Number'));
+
+                                    }
+                                        , null,
+
+                                        function (json) {
+
+                                            //error
+
+                                        });
+                                    ele('GRN_Number').setValue('');
+                                    ele('Package_Number').setValue('');
+                                    ele('Location_Code').setValue('');
+                                    webix.UIManager.setFocus(ele('GRN_Number'));
+
+                                }
+
+                                else if (webix.UIManager.getNext(view).config.type == 'line') {
+
+                                    webix.UIManager.setFocus(webix.UIManager.getNext(webix.UIManager.getNext(view)));
+
+                                    view.disable();
+
+                                }
+
+                                else {
+
+                                    webix.UIManager.setFocus(webix.UIManager.getNext(view));
+
+                                }
+
+                            }
+
+                        },
+                        elements:
+                            [
+                                {
+                                    cols:
+                                        [
+                                            {
+                                                cols: [
+                                                    vw1("text", 'GRN_Number', "GRN Number", {
+                                                        //required: true, suggest: fd + "?type=1", width: 250,
+                                                    }),
+                                                    vw1("text", 'Package_Number', "Package Number", {
+                                                        //required: true, suggest: fd + "?type=1", width: 250,
+                                                    }),
+                                                    vw1("text", 'Location_Code', "Location Code", {
+                                                        //required: true, suggest: fd + "?type=1", width: 250,
+                                                    }),
+                                                ]
+
+                                            },
+                                            {}
+                                        ]
+                                },
+                                {
+                                    padding: 3,
+                                    cols: [
+                                        {
+                                            view: "datatable", id: $n("dataT1"), navigation: true, select: true, editaction: "custom",
+                                            resizeColumn: true, autoheight: false, multiselect: true, hover: "myhover",
+                                            threeState: true, rowLineHeight: 25, rowHeight: 25,
+                                            datatype: "json", headerRowHeight: 25, leftSplit: 2, editable: true,
+                                            scheme:
+                                            {
+                                                $change: function (obj) {
+                                                    var css = {};
+                                                    obj.$cellCss = css;
+                                                }
+                                            },
+                                            columns: [
+                                                { id: "NO", header: "No.", css: "rank", width: 50, sort: "int" },
+                                                { id: "GRN_Number", header: ["GRN Number", { content: "textFilter" }], width: 200 },
+                                                { id: "Package_Number", header: ["Package Number", { content: "textFilter" }], width: 150 },
+                                                { id: "FG_Serial_Number", header: ["Serial Number", { content: "textFilter" }], width: 150 },
+                                                { id: "Qty", header: ["Qty", { content: "textFilter" }], width: 100 },
+                                                { id: "Area", header: ["Area", { content: "textFilter" }], width: 150 },
+                                                { id: "Location_Code", header: ["Location Code", { content: "textFilter" }], width: 150 },
+                                            ],
+                                            onClick:
+                                            {
+                                            },
+                                            on: {
+                                                // "onEditorChange": function (id, value) {
+                                                // }
+                                                "onItemClick": function (id) {
+                                                    this.editRow(id);
+                                                }
+                                            }
+                                        },
+                                    ]
+                                },
+
+                            ]
+                    }
+                ], on:
             {
-                onHide:function()
-                {
-                    
-                },
-                onShow:function()
-                {
+                onHide: function () {
 
                 },
-                onAddView:function()
-                {
-                	init();
+                onShow: function () {
+
+                },
+                onAddView: function () {
+                    init();
                 }
             }
         }
--- Receiving/PutAway_data.php
+++ Receiving/PutAway_data.php
@@ -1,78 +1,181 @@
 <?php
-if(!ob_start("ob_gzhandler")) ob_start();
+if (!ob_start("ob_gzhandler")) ob_start();
 header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
 header('Cache-Control: no-store, no-cache, must-revalidate');
 header('Cache-Control: post-check=0, pre-check=0', FALSE);
 header('Pragma: no-cache');
 include('../start.php');
 session_start();
-if(!isset($_SESSION['xxxID']) || !isset($_SESSION['xxxRole']) || !isset($_SESSION['xxxID']) || !isset($_SESSION['xxxFName'])  || !isset($_SESSION['xxxRole']->{'PutAway'}) )
-{
+if (!isset($_SESSION['xxxID']) || !isset($_SESSION['xxxRole']) || !isset($_SESSION['xxxID']) || !isset($_SESSION['xxxFName'])  || !isset($_SESSION['xxxRole']->{'PutAway'})) {
 	echo "{ch:10,data:'เวลาการเชื่อมต่อหมด<br>คุณจำเป็นต้อง login ใหม่'}";
 	exit();
-}
-else if($_SESSION['xxxRole']->{'PutAway'}[0] == 0)
-{
+} else if ($_SESSION['xxxRole']->{'PutAway'}[0] == 0) {
 	echo "{ch:9,data:'คุณไม่ได้รับอุญาติให้ทำกิจกรรมนี้'}";
 	exit();
 }
 
-if(!isset($_REQUEST['type'])) {echo json_encode(array('ch'=>2,'data'=>'ข้อมูลไม่ถูกต้อง'));exit();}
+if (!isset($_REQUEST['type'])) {
+	echo json_encode(array('ch' => 2, 'data' => 'ข้อมูลไม่ถูกต้อง'));
+	exit();
+}
 $cBy = $_SESSION['xxxID'];
 $fName = $_SESSION['xxxFName'];
 $type  = intval($_REQUEST['type']);
 
 
 include('../php/connection.php');
-if($type<=10)//data
+if ($type <= 10) //data
 {
-	if($type == 1)
-	{
-	}
-	else closeDBT($mysqli,2,'TYPE ERROR');
-}
-else if($type>10 && $type<=20)//insert
-{
-	if($_SESSION['xxxRole']->{'PutAway'}[1] == 0) closeDBT($mysqli,9,'คุณไม่ได้รับอุญาติให้ทำกิจกรรมนี้');
-	if($type == 11)
-	{
+	if ($type == 1) {
+		$dataParams = array(
+			'obj',
+			'obj=>GRN_Number:s:0:0',
+			'obj=>Package_Number:s:0:0',
+			'obj=>Location_Code:s:0:0',
+		);
+		$chkPOST = checkParamsAndDelare($_POST, $dataParams, $mysqli);
+		if (count($chkPOST) > 0) closeDBT($mysqli, 2, join('<br>', $chkPOST));
 
-	}
-	else if($type == 12)
-	{
+		$sql = "SELECT GRN_Number,
+		Package_Number,
+		ts.Serial_Number,
+		Qty,
+		Area,
+		Location_Code
+		FROM tbl_transaction ts
+		inner join tbl_receiving_header trh on ts.Receiving_Header_ID = trh.Receiving_Header_ID
+		left join tbl_location_master tlm on ts.To_Loc_ID = tlm.Location_ID
+		where GRN_Number = '$GRN_Number' and Package_Number = '$Package_Number';";
+		$re1 = sqlError($mysqli, __LINE__, $sql, 1);
 
-	}
-	else closeDBT($mysqli,2,'TYPE ERROR');
-}
-else if($type>20 && $type<=30)//update
+		closeDBT($mysqli, 1, jsonRow($re1, true, 0));
+	} else closeDBT($mysqli, 2, 'TYPE ERROR');
+} else if ($type > 10 && $type <= 20) //insert
 {
-	if($_SESSION['xxxRole']->{'PutAway'}[2] == 0) closeDBT($mysqli,9,'คุณไม่ได้รับอุญาติให้ทำกิจกรรมนี้');
-	if($type == 21)
-	{
-		
-	}
-	else closeDBT($mysqli,2,'TYPE ERROR');
-}
-else if($type>30 && $type<=40)//delete
+	if ($_SESSION['xxxRole']->{'PutAway'}[1] == 0) closeDBT($mysqli, 9, 'คุณไม่ได้รับอุญาติให้ทำกิจกรรมนี้');
+	if ($type == 11) {
+	} else if ($type == 12) {
+	} else closeDBT($mysqli, 2, 'TYPE ERROR');
+} else if ($type > 20 && $type <= 30) //update
 {
-	if($_SESSION['xxxRole']->{'PutAway'}[3] == 0) closeDBT($mysqli,9,'คุณไม่ได้รับอุญาติให้ทำกิจกรรมนี้');
-	if($type == 31)
-	{
+	if ($_SESSION['xxxRole']->{'PutAway'}[2] == 0) closeDBT($mysqli, 9, 'คุณไม่ได้รับอุญาติให้ทำกิจกรรมนี้');
+	if ($type == 21) {
+		$dataParams = array(
+			'obj',
+			'obj=>GRN_Number:s:0:1',
+			'obj=>Package_Number:s:0:1',
+			'obj=>Location_Code:s:0:1',
+		);
+		$chkPOST = checkParamsAndDelare($_POST, $dataParams, $mysqli);
+		if (count($chkPOST) > 0) closeDBT($mysqli, 2, join('<br>', $chkPOST));
 
-	}
-	else closeDBT($mysqli,2,'TYPE ERROR');
-}
-else if($type>40 && $type<=50)//save
+		$mysqli->autocommit(FALSE);
+		try {
+
+			$sql = "SELECT
+			Receiving_Header_ID
+			from tbl_receiving_header
+			where GRN_Number = '$GRN_Number' and Status_Receiving = 'COMPLETE'";
+			$re1 = sqlError($mysqli, __LINE__, $sql, 1);
+			if ($re1->num_rows == 0) {
+				throw new Exception('ไม่พบข้อมูล' . __LINE__);
+			}
+			$Receiving_Header_ID = $re1->fetch_array(MYSQLI_ASSOC)['Receiving_Header_ID'];
+
+
+			$sql = "SELECT
+			Location_ID
+			from tbl_location_master where Location_Code = '$Location_Code'";
+			$re1 = sqlError($mysqli, __LINE__, $sql, 1);
+			if ($re1->num_rows == 0) {
+				throw new Exception('ไม่พบข้อมูล' . __LINE__);
+			}
+
+			$sql = "SELECT
+			Location_ID,
+			Area
+			from tbl_location_master where Location_Code = '$Location_Code' and Area = 'Storage';";
+			$re1 = sqlError($mysqli, __LINE__, $sql, 1);
+			if ($re1->num_rows == 0) {
+				throw new Exception('Location นี้ไม่อยู่ใน Area Storage' . __LINE__);
+			}
+			while ($row = $re1->fetch_array(MYSQLI_ASSOC)) {
+				$Area = $row['Area'];
+				$Location_ID = $row['Location_ID'];
+			}
+			
+			$sql = "SELECT
+			Area,
+			Location_ID
+			from tbl_inventory
+			where Receiving_Header_ID = '$Receiving_Header_ID'";
+			$re1 = sqlError($mysqli, __LINE__, $sql, 1);
+			if ($re1->num_rows == 0) {
+				throw new Exception('ไม่พบข้อมูล' . __LINE__);
+			}
+			while ($row = $re1->fetch_array(MYSQLI_ASSOC)) {
+				$Area_receive = $row['Area'];
+				$Location_ID_Receive = $row['Location_ID'];
+			}
+
+			//อัพเดท Area ใน tbl_inventory
+			$sql = "UPDATE tbl_inventory tiv
+			left join tbl_receiving_header trh on tiv.Receiving_Header_ID = trh.Receiving_Header_ID
+			set tiv.Area = 'Storage'
+			where tiv.Receiving_Header_ID = '$Receiving_Header_ID' and tiv.Package_Number = '$Package_Number' and trh.Status_Receiving = 'COMPLETE';";
+			sqlError($mysqli, __LINE__, $sql, 1);
+			if ($mysqli->affected_rows == 0) {
+				throw new Exception('ไม่สามารถบันทึกข้อมูลได้' . __LINE__);
+			}
+
+			//อัพเดท tbl_transaction
+			$sql = "UPDATE tbl_transaction ts
+			left join tbl_receiving_header trh on ts.Receiving_Header_ID = trh.Receiving_Header_ID
+			set From_Area = '$Area_receive',
+			To_Area = '$Area',
+			Trans_Type = 'PUT AWAY',
+			From_Loc_ID = '$Location_ID_Receive',
+			To_Loc_ID = '$Location_ID'
+			where ts.Receiving_Header_ID = '$Receiving_Header_ID' and ts.Package_Number = '$Package_Number' and trh.Status_Receiving = 'COMPLETE';";
+			sqlError($mysqli, __LINE__, $sql, 1);
+			if ($mysqli->affected_rows == 0) {
+				throw new Exception('ไม่สามารถบันทึกข้อมูลได้' . __LINE__);
+			}
+			
+
+			$mysqli->commit();
+
+			$sql = "SELECT GRN_Number,
+			Package_Number,
+			ts.Serial_Number,
+			Qty,
+			Area,
+			Location_Code
+			FROM tbl_transaction ts
+			inner join tbl_receiving_header trh on ts.Receiving_Header_ID = trh.Receiving_Header_ID
+			left join tbl_location_master tlm on ts.To_Loc_ID = tlm.Location_ID
+			where GRN_Number = '$GRN_Number' and Package_Number = '$Package_Number';";
+			$re1 = sqlError($mysqli, __LINE__, $sql, 1);
+			closeDBT($mysqli, 1, jsonRow($re1, true, 0));
+		} catch (Exception $e) {
+			$mysqli->rollback();
+			closeDBT($mysqli, 2, $e->getMessage());
+		}
+
+
+		closeDBT($mysqli, 1, jsonRow($re1, true, 0));
+	} else closeDBT($mysqli, 2, 'TYPE ERROR');
+} else if ($type > 30 && $type <= 40) //delete
 {
-	if($_SESSION['xxxRole']->{'PutAway'}[1] == 0) closeDBT($mysqli,9,'คุณไม่ได้รับอุญาติให้ทำกิจกรรมนี้');
-	if($type == 41)
-	{
-		
-	}
-	else closeDBT($mysqli,2,'TYPE ERROR');
-}
-else closeDBT($mysqli,2,'TYPE ERROR');
+	if ($_SESSION['xxxRole']->{'PutAway'}[3] == 0) closeDBT($mysqli, 9, 'คุณไม่ได้รับอุญาติให้ทำกิจกรรมนี้');
+	if ($type == 31) {
+	} else closeDBT($mysqli, 2, 'TYPE ERROR');
+} else if ($type > 40 && $type <= 50) //save
+{
+	if ($_SESSION['xxxRole']->{'PutAway'}[1] == 0) closeDBT($mysqli, 9, 'คุณไม่ได้รับอุญาติให้ทำกิจกรรมนี้');
+	if ($type == 41) {
+	} else closeDBT($mysqli, 2, 'TYPE ERROR');
+} else closeDBT($mysqli, 2, 'TYPE ERROR');
 
 $mysqli->close();
 exit();
-?>
